4d74bb31fd44313dba57ba384da23b402ea4b964
Arnold Daniels <arnold@jasny.net>
2012-10-19 22:34:32 +0200

    Show markdown in GitHub style

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Markdown Handler
 This little script adds an Apache handler that lets you view Markdown files with a .md extension
 more prettily on your webserver.
 
+The Markdown is styled as it would at GitHub.
+
 It's based on a PHP Markdown implementation by [Michel Fortin](http://www.michelf.com/).
 
 Installation
--- markdown/handler.php
+++ markdown/handler.php
@@ -1,5 +1,20 @@
 <?php
+require('markdown.php');
+
 header('Content-type: text/html; charset=utf-8');
+
+$legalExtensions = array('md', 'markdown');
+
+$file = realpath($_SERVER['PATH_TRANSLATED']);
+if($file
+	&& in_array(strtolower(substr($file,strrpos($file,'.')+1)), $legalExtensions)
+	&& substr($file,0,strlen($_SERVER['DOCUMENT_ROOT'])) == $_SERVER['DOCUMENT_ROOT']) {
+	$content = Markdown(file_get_contents($file));
+} else {
+	$content = "<p>Bad filename given</p>";
+}
+
+$filename = basename($file);
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,20 +23,13 @@ header('Content-type: text/html; charset=utf-8');
 	<meta name="content-type" http-equiv="content-type" value="text/html; utf-8">
 </head>
 <body>
-<?php
-
-require('markdown.php');
-
-$legalExtensions = array('md', 'markdown');
-
-$file = realpath($_SERVER['PATH_TRANSLATED']);
-if($file
-	&& in_array(strtolower(substr($file,strrpos($file,'.')+1)), $legalExtensions)
-	&& substr($file,0,strlen($_SERVER['DOCUMENT_ROOT'])) == $_SERVER['DOCUMENT_ROOT']) {
-	echo Markdown(file_get_contents($file));
-} else {
-	echo "<p>Bad filename given</p>";
-}
-?>
+<div class="container">
+  <div id="readme" class="clearfix announce instapaper_body md" data-path="/" style="display: block; ">
+    <span class="name"><span class="mini-icon mini-icon-readme"></span> <?= $filename ?></span>
+    <article class="markdown-body entry-content">
+      <?= $content ?>
+    </article>
+  </div>
+</div>
 </body>
 </html>
